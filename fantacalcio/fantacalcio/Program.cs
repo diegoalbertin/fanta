ff9f1be575c0c2a0cf545770bcca779a18b352f1
diegoalbertin <72445590+diegoalbertin@users.noreply.github.com>
2021-11-05 09:23:06 +0100

    .

--- fantacalcio/fantacalcio/Program.cs
+++ fantacalcio/fantacalcio/Program.cs
@@ -8,6 +8,8 @@ namespace fantacalcio
 {
     class Program
     {
+        static public bool accessoEseguito = false;//variabile per indicare se l'utente 
+        static public int numeroOggettoPlayer;
         static public string nomeCalciatore = "";
         static public string cognomeCalciatore = "";
         static public int prezzoCalciatore = 0;
@@ -29,8 +31,15 @@ namespace fantacalcio
             while (cicloInfinito==0)
             {
                 Console.WriteLine("BENVENUTO NEL GIOCO FANTACALCIO");
-                sceltaUtenteNoLogIn();
-                Console.ReadKey();
+                if (accessoEseguito == false)
+                {
+                    sceltaUtenteNoLogIn();
+                }
+                else if (accessoEseguito == true)
+                {
+                    sceltaUtenteLogIn();
+                }
+                Console.ReadKey();               
             }
         }
         static public void sceltaUtenteNoLogIn()//funzione che permette all'utente di eseguire una tra le azioni base 
@@ -64,7 +73,54 @@ namespace fantacalcio
                 Console.Clear();
                 visualizzaClassifica();
             }
-            else if (nScelta == 4)
+            else if (nScelta == 4)//scelta n°4
+            {
+                Console.WriteLine("chiusura programma");
+                Environment.Exit(0);
+            }
+        }
+        static public void sceltaUtenteLogIn()
+        {
+            Console.WriteLine("cosa vorresti fare?inserisci il numero corrispondente alla tua scelta" + "\n" + "1-inizia asta, 2-log out, 3-visualizza classifica, 4-visualizza la tua rosa, 5-chiudi programma");
+            string inserimentoSceltaUtente = Console.ReadLine();
+            int nScelta = 0;
+            bool success = int.TryParse(inserimentoSceltaUtente, out nScelta);//viene effettuato un controllo sull'inserimento della scelta
+            if (success == false)//in caso non sia stato inserito un numero
+            {
+                Console.WriteLine("non hai inserito un numero");
+                sceltaUtenteNoLogIn();
+            }
+            else if (nScelta != 1 && nScelta != 2 && nScelta != 3 && nScelta != 4)//in caso non sia stato inserito un numero compreso tra 1-3
+            {
+                Console.WriteLine("non hai inserito un numero tra quelli elencati");
+                sceltaUtenteNoLogIn();
+            }
+            else if (nScelta == 1)//scelta n°1
+            {
+                Console.Clear();
+                iniziaAsta();
+            }
+            else if (nScelta == 2)//scelta n°2
+            {
+                Console.Clear();
+                logOut();
+            }
+            else if (nScelta == 3)//scelta n°3
+            {
+                Console.Clear();
+                visualizzaClassifica();
+            }
+            else if (nScelta==4)
+            {
+                Console.Clear();
+                visualizzaRosaGiocatore();
+            }
+            else if (nScelta == 5)
+            {
+                Console.Clear();
+                inserisciRosaProssimaGiornata();
+            }
+            else if (nScelta == 6)
             {
                 Console.WriteLine("chiusura programma");
                 Environment.Exit(0);
@@ -72,29 +128,38 @@ namespace fantacalcio
         }
         static public void creazioneID()//funzione che crea l'ID
         {
-            string variabileSceltaUtente = "";
+            string variabileSceltaUtente = "";//variabile utilizzata per la scelta dell'azione dell'utente
             Console.WriteLine("CREAZIONE ID" );
-            nome_Password();
-            Console.WriteLine("sei sicuro di voler creare un nuovo ID con queste credenziali?" + "\n" + "username e password non saranno modificabili dopo la creazione dell'ID" + "\n" + "se vuoi interrompere la creazione digita NO, altrimenti digita SI");
-            while (variabileSceltaUtente!="si"&& variabileSceltaUtente !="SI"&& variabileSceltaUtente !="no" && variabileSceltaUtente != "NO")
+            nome_Password();//viene richiamata la funzione per l'inserimento di nome e passord da parte dell'utente
+            for (int i = 0; i < numeroPlayer; i++)//ciclo for per "puntare" al singolo oggetto della classe player
             {
-                Console.WriteLine("scelta non valida, digita SI oppure NO");
+                if (player[i].nome == nomePlayer)//ciclo if per il controllo dell'unicità del nome inserito dall'utente 
+                {
+                    Console.WriteLine("nome utente già in uso" + "\n" + "inserirne uno valido");
+                    Console.Clear();
+                    creazioneID();//in caso esista già un utente con quel nome viene richiamata la funzione per la creazione dell'ID
+                }
+            }
+            Console.WriteLine("sei sicuro di voler creare un nuovo ID con queste credenziali?" + "\n" + "username e password non saranno modificabili dopo la creazione dell'ID" + "\n" + "se vuoi interrompere la creazione digita NO, altrimenti digita SI");
+            while (variabileSceltaUtente!="si"&& variabileSceltaUtente !="SI"&& variabileSceltaUtente !="no" && variabileSceltaUtente != "NO")//ciclo while per controllare se l'utente digita una parola accettabile dal programma
+            {
+                Console.WriteLine("scelta non valida, digita SI oppure NO");//in caso sbagli nell'inserimento gli viene richiesto nuovamente
                 variabileSceltaUtente = Console.ReadLine();
             }
-            if (variabileSceltaUtente == "si" || variabileSceltaUtente == "SI" || variabileSceltaUtente == "Si")
+            if (variabileSceltaUtente == "si" || variabileSceltaUtente == "SI" || variabileSceltaUtente == "Si")//se la scelta è si
             {
-                numeroPlayer++;
+                numeroPlayer++;//viene aumentato il numero di giocatori
                 player[numeroPlayer - 1] = new player();//viene istanziato un nuovo oggetto della classe player e ne vengono richiamati due metodi
                 player[numeroPlayer - 1].getNome();
                 player[numeroPlayer - 1].getPassword();
                 righeFile++;//aumenta di 1 il numero delle righe del file
-                string variabiliNonImpostate = "0";
+                string variabiliNonImpostate = "0";//stringa per le variabili che dovranno essere impostate in futuro
                 string[,] arrayDiSalvataggio = new string[righeFile, 4];//array utilizzato per il salvataggio su File
                 for (int i = 0; i < righeFile; i++)//ciclo che inserisce i valori nell'array di salvataggio
                 {
                     if (i == 0)//se i=0, quindi si sta lavorando della prima riga dell'array, salva il nuovo ID con punteggio=0
                     {
-                        arrayDiSalvataggio[i, 0] = nomePlayer + carattereDivisore;
+                        arrayDiSalvataggio[i, 0] = nomePlayer + carattereDivisore;//le varie colonne dell'array vengono impostate con le caratteristiche del nuovo utente
                         arrayDiSalvataggio[i, 1] = passwordPlayer + carattereDivisore;
                         arrayDiSalvataggio[i, 2] = variabiliNonImpostate + carattereDivisore;
                         arrayDiSalvataggio[i, 3] = variabiliNonImpostate + carattereDivisore + "\n";
@@ -138,21 +203,42 @@ namespace fantacalcio
                 Console.Clear();
             }
         }
-        static public void logIn()
+        static public void logIn()//funzione per il log in
         {
-            nome_Password();
-
+            nome_Password();//viene richiamata la funzione per l'inserimento di nome e passord da parte dell'utente
+            for (int i = 0; i < numeroPlayer; i++)//ciclo for per "puntare" al singolo oggetto della classe player
+            {
+                if (player[i].nome==nomePlayer&&player[i].password==passwordPlayer)//ciclo if che effetua il controllo tra i nomi e le password
+                {           //se viene trovata una corrispondenza 
+                    Console.WriteLine("login effettuato");
+                    accessoEseguito = true;//variabile che indica se il log in è stato eseguito impostata a true
+                    numeroOggettoPlayer = i;//viene salvato il numero relativo all'oggetto player
+                    break;
+                }
+                break;
+            }
         }
         static public void visualizzaClassifica()
         {
-
+            //coming soon
         }
-        static public void nome_Password()
+        static public void logOut()//funzione per il log out
         {
-            Console.WriteLine("qual'e' il tuo username?");
-            nomePlayer = Console.ReadLine();
-            Console.WriteLine("qual'e' sarà la tua password?");
-            passwordPlayer = Console.ReadLine();
+            accessoEseguito = false;//variabile che indica se il log in è stato eseguito impostata a false
+            numeroOggettoPlayer = numeroPlayer;//il numero relativo all'oggetto player viene impostato su un valore impossibile
+        }
+        static public void nome_Password()//funzione per l'inserimento di nome e password del player
+        {
+            string scelta = "NO";//la stringa viene impostata a NO per poter far partire il ciclo successivo
+            while(scelta != "si" || scelta != "SI" || scelta != "Si")//se la scelta dell'utente non è SI esegue il ciclo
+            {
+                Console.WriteLine("qual'e' il tuo username?");
+                nomePlayer = Console.ReadLine();//inserimento del nome
+                Console.WriteLine("qual'e' sarà la tua password?");
+                passwordPlayer = Console.ReadLine();//inserimento della password
+                Console.WriteLine("se le credenziali vanno bene digita SI, altrimenti digita NO per reinserirle");
+                scelta = Console.ReadLine();//variabile per l'eventuale reinserimento di nome e cognome 
+            }
         }
         private static void acquisizioneFileOggetti()//funzione che gestisce l'eventuale creazione e caricamento di dati presenti su file nel programma
         {
@@ -240,8 +326,8 @@ namespace fantacalcio
     }
     class player
     {
-        string nome = "";
-        string password = "";
+        public string nome = "";
+        public string password = "";
         public player()
         {
 
